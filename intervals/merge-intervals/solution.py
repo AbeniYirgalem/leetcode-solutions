261ead3fea5a423c67abb077c59ea9f4fa75ee4e
Abeni <abenezeryirgalem0@gmail.com>
2026-06-01 09:14:04 +0300

    implement merge method to combine overlapping intervals

---
+++ intervals/merge-intervals/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            if start <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], end)
+            else:
+                merged.append([start, end])
+        return merged
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+    print(sol.merge([[1, 4], [4, 5]]))
+    print(sol.merge([[1, 2]]))
