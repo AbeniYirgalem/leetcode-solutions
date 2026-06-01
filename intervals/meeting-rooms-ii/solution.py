ff13e2296cc0156f8333a884b93af4b2b7350445
Abeni <abenezeryirgalem0@gmail.com>
2026-06-01 09:13:41 +0300

    implement minMeetingRooms method to calculate the minimum number of meeting rooms required

---
+++ intervals/meeting-rooms-ii/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda x: x[0])
+        heap: List[int] = []
+
+        for start, end in intervals:
+            if heap and heap[0] <= start:
+                heapq.heapreplace(heap, end)
+            else:
+                heapq.heappush(heap, end)
+        return len(heap)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minMeetingRooms([[0, 30], [5, 10], [15, 20]]))
+    print(sol.minMeetingRooms([[7, 10], [2, 4]]))
+    print(sol.minMeetingRooms([[1, 2], [2, 3]]))
