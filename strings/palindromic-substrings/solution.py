2d6ddf4b9b7c4589b17283c0a5b24278e8646841
Abeni <abenezeryirgalem0@gmail.com>
2026-06-12 19:41:58 +0300

    feat(string): implement Palindromic Substrings using expand-around-center

---
+++ strings/palindromic-substrings/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def expand(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
+
+        total = 0
+        for i in range(len(s)):
+            total += expand(i, i)
+            total += expand(i, i + 1)
+        return total
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.countSubstrings("aaa"))
+    print(sol.countSubstrings("abc"))
+    print(sol.countSubstrings("a"))
